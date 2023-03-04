30d5713d71a0cd20b646310ba7e267e2eaebd393
ogb-daniel <ogbuikedaniel@gmail.com>
2023-03-04 23:32:00 +0100

    get movie logic

--- server/controllers/movies.ts
+++ server/controllers/movies.ts
@@ -23,6 +23,16 @@ export const addMovie = async(req:Request,res:Response)=>{
     }
 }
 
+export const getMovieById = async(req:Request,res:Response)=>{
+    const {movieId:_id} = req.params;
+    try {
+        const movie = await Movie.findById(_id);
+        res.status(200).json(movie)
+    } catch (error:any) {
+        res.status(404).json({message:error.message})        
+    }
+}
+
 export const searchMovie = async(req:Request,res:Response)=>{
     
     const {title,genre} = req.body;
